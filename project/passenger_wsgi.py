9f0f8da7987ad19707d82dbc3c073610afef3848
Seung-jin Kim <seungjin@georgetown.dreamhost.com>
2010-08-27 16:10:10 -0700

    dreamhost django done

--- project/passenger_wsgi.py
+++ project/passenger_wsgi.py
@@ -1,15 +1,16 @@
-
+#!/usr/env python
 
 import sys, os
+
 INTERP = "/home/seungjin/webapps/c.seungjin.net/bin/python"
 
-if sys.executable != INTERP: 
-	os.execl(INTERP, INTERP, *sys.argv)
+if sys.executable != INTERP:
+  os.execl(INTERP, INTERP, *sys.argv)
 
-sys.path.append("/home/seungjin/c.seungjin.net/project")
+sys.path.append("/home/seungjin/webapps/c.seungjin.net/project")
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "project.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
-import django.core.handlers.wsgi
+import django.core.handlers.wsgi  
 
-application = django.core.handlers.wsgi.WSGIHandler()
+application = django.core.handlers.wsgi.WSGIHandler() 
