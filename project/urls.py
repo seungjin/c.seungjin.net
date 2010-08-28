c02cb6e87cb29343412d3d98c3a5632c66f789bd
seungjin <seungjin@seungjin.net>
2010-08-27 22:50:04 -0700

    //

--- project/urls.py
+++ project/urls.py
@@ -23,8 +23,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     #(r'^admin/', include(admin.site.urls)),
     
+    (r'^robots\.txt$', 'django.views.generic.simple.direct_to_template', {'template': 'robots.txt', 'mimetype': 'text/plain'}),
     (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/site_media/favicon.ico'}),
-    (r'^robots\.txt$', 'django.views.generic.simple.redirect_to', {'url': '/site_media/robots.txt'}),
     
     (r'^$', main),
     (r'^journals$', main),
