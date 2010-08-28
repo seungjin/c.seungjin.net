57652788ac1c64515930135a967f2f9555d17431
seungjin <seungjin@seungjin.net>
2010-08-27 20:10:10 -0700

    django orm select and order by

--- project/journals/views.py
+++ project/journals/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 from journals.models import *
 
 def main(request):
+  journals = Journals.objects.order_by('-id').all()
+  for journal in journals.all() :
+    print journal.subject
   return HttpResponse("hi")
