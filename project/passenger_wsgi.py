6d6a4479323a13fabca00464166a4c8efbe8483f
Seung-jin Kim <seungjin@georgetown.dreamhost.com>
2010-08-29 10:44:31 -0700

    ..

--- project/passenger_wsgi.py
+++ project/passenger_wsgi.py
@@ -2,10 +2,10 @@
 
 import sys, os
 
-#this_directory = "/home/seungjin/webapps/c.seungjin.net/project"
-this_directory = os.path.dirname(__file__)
+this_directory = "/home/seungjin/webapps/c.seungjin.net/project"
+#this_directory = os.path.dirname(__file__)
 
-INTERP = this_directory + "/../bin/python"
+INTERP = os.path.join(this_directory,"../bin/python")
 
 if sys.executable != INTERP:
   os.execl(INTERP, INTERP, *sys.argv)
