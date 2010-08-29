27ab3fb13dc04a932d2bb23c8b192e15152c3030
seungjin <seungjin@email.arizona.edu>
2010-08-28 19:32:48 -0700

    access_log works

--- project/journal/views.py
+++ project/journal/views.py
@@ -4,11 +4,27 @@ from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import get_template
 
-from time import gmtime, strftime
+from time import gmtime, strftime, localtime, tzname
 
 from journal.models import *
 
+def access_log(request):
+  if request.META.has_key('HTTP_REFERER') :
+    http_referer = request.META['HTTP_REFERER']
+  else:
+    http_referer = None
+  acessLog = AccessLogs(
+    timestamp = strftime("%Y-%m-%d %H:%M:%S",localtime()),
+    timezone = tzname[0],
+    remote_ip = request.META['REMOTE_ADDR'],
+    user_agent = request.META['HTTP_USER_AGENT'],
+    url = request.META['SERVER_PROTOCOL'].split("/")[0].lower() + "://" + request.META['HTTP_HOST'] + request.META['PATH_INFO'],
+    http_referer = http_referer
+  )
+  acessLog.save()
+
 def main(request):
+  access_log(request)
   journals = Journals.objects.filter(publishing_code=1).order_by('-id').all()
   variables = Context({
     'http_host' : request.META['HTTP_HOST'],
@@ -20,6 +36,7 @@ def main(request):
   return HttpResponse(output)
   
 def view_with_id(request, id):
+  access_log(request)
   journals = Journals.objects.filter(publishing_code=1).filter(id=id).order_by('-id').all()
   variables = Context({
     'current_time' : strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()),
