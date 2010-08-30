8a851d66cfa0a182cf70f1c3ffb9e60b254c3393
seungjin <seungjin@email.arizona.edu>
2010-08-30 15:50:20 -0700

    ..

--- project/urls.py
+++ project/urls.py
@@ -32,9 +32,9 @@ urlpatterns = patterns('',
 
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media}),
 
-    (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/recent/100'}),
+    (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/journal/recent/100'}),
     
-    (r'^journal/recent$', 'django.views.generic.simple.redirect_to', {'url': '/recent/100'}),
+    (r'^journal/recent$', 'django.views.generic.simple.redirect_to', {'url': '/journal/recent/100'}),
     (r'^journal/recent/(?P<size>\d+)/$', recent),
     
     (r'^journal/all$', all),
